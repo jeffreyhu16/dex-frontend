34d2be692e8dfcc26fe8eccc1d0d85df03696618
Jeffrey <jeffreyhu16@gmail.com>
2022-09-02 12:22:26 +0800

    add account balance

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png'
+import Blockies from 'react-blockies';
 
 export default function Navbar() {
 
-    const account = useSelector(state => state.provider.account);
+    const provider = useSelector(state => state.provider);
+
+    let truncatedAcc, roundedBalance;
+    if (provider.account) {
+        const { account, balance } = provider;
+        truncatedAcc = account.slice(0, 5) + '...' + account.slice(account.length - 4);
+        roundedBalance = Number(balance).toFixed(4);
+    }
 
     return (
         <div className='exchange__header grid'>
@@ -18,7 +26,14 @@ export default function Navbar() {
             </div>
 
             <div className='exchange__header--account flex'>
-                <div>{account}</div>
+                <p>
+                    <small>Balance</small>
+                    {roundedBalance}
+                </p>
+                <a href=''>
+                    {truncatedAcc}
+                    <Blockies seed={provider.account ? provider.account : ''} className='identicon' />
+                    </a>
             </div>
         </div>
     )
