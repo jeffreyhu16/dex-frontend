03821e927d068991d6594f9bb3d8e8cca05ce4ce
Jeffrey <jeffreyhu16@gmail.com>
2022-09-10 19:05:35 +0800

    change state name

--- src/redux/tokenSlice.js
+++ src/redux/tokenSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const tokenSlice = createSlice({
-    name: 'token',
+    name: 'tokens',
     initialState: {
         symbols: [],
     },
